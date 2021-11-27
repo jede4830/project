c7d9ca889b87df78dcba4f63023e8510a45e0505
Jenna Dean <69818454+jede4830@users.noreply.github.com>
2021-11-27 16:40:12 -0600

    fixed doitall

--- doitall.sh
+++ doitall.sh
@@ -2,9 +2,9 @@
 ./install.sh
 ./deploy-local-dev.sh
 ./port-forwarding.sh
-kubectl apply -f worker/worker-deployment.yaml
+k3s kubectl apply -f worker/worker-deployment.yaml
 echo "Sleeping for 60 seconds..."
 sleep 60
-kubectl apply -f rest/rest-deployment.yaml
-kubectl apply -f rest/rest-service.yaml
+k3s kubectl apply -f rest/rest-deployment.yaml
+k3s kubectl apply -f rest/rest-service.yaml
 k3s kubectl port-forward --address 0.0.0.0 service/project-rest-svc 5000:5000 
